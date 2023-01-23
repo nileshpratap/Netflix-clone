3d64cd05fcabb36323e4bab4ac7edfed8b2d6699
nileshpratap <nileshpratap03@gmail.com>
2023-01-23 14:26:33 +0530

    Done with Banner Component

--- src/Banner.js
+++ src/Banner.js
@@ -46,6 +46,7 @@ function Banner() {
         </div>
         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
+      <div className="banner--fadeBottom"></div>
     </header>
   );
 }
