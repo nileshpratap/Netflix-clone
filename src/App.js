de939f66a9632c7a7cc6d74aac7473a9537e9fb0
nileshpratap <nileshpratap03@gmail.com>
2023-01-23 11:23:39 +0530

    Implemented Row component and working on Banner Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Banner from "./Banner";
 import Row from "./Row";
 import requests from "./requests";
 
@@ -8,12 +9,23 @@ import requests from "./requests";
 function App() {
   return (
     <div className="App">
+      {/* navbar */}
+      <Banner />
+      {/* top poster */}
+
       {/* building the rows */}
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
       />
       <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       <Row title="Popular" fetchUrl={requests.fetchPopular} />
     </div>
   );
